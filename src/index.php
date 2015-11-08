db1022b81bc2e6c2d4ef3fbd688d62c7c2cc4898
Adam Nicholson <adamnicholson10@gmail.com>
2015-11-08 20:13:21 +0000

    Add google analytics

--- src/index.php
+++ src/index.php
@@ -47,4 +47,14 @@
     <h3>Probably about <?= (new \Carbon\Carbon('2015-11-15'))->diffForHumans(); ?></h3>
 </small>
 
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-69849930-1', 'auto');
+    ga('send', 'pageview');
+</script>
+
 </html>
